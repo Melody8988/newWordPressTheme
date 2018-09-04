d1a6de0005e91fcc63f57e03ca42c018682d34f6
Melody8988 <melody.massard8988@gmail.com>
2018-09-04 11:21:10 -0500

    menu issue, author not showing

--- functions.php
+++ functions.php
@@ -1,19 +1,21 @@
 <?php
 
-function newTheme_script_enqueue_melody8988(){
+function newTheme_script_enqueue(){
     wp_enqueue_style( 'customstyle', get_template_directory_uri() . '/css/newTheme.css', array(), 'all'); //TODO: include version
     wp_enqueue_script( 'customsjs', get_template_directory_uri() . '/js/newTheme.js', array(), true ); //TODO: include version
 }
 
-add_action( 'wp_enqueue_scripts', 'newTheme_script_enqueue_melody8988');
+add_action( 'wp_enqueue_scripts', 'newTheme_script_enqueue');
 //'wp_enqueue_scripts'
 
-// function newTheme_theme_setup_melody8988(){
-
-//     add_theme_support( 'menus' );
+function newTheme_theme_setup(){
+    
+    add_theme_support( 'menus' );
      
-//     register_nav_menu( 'primary', 'Primary Header Navigation' );
+    register_nav_menu( 'primary', 'Primary Header Navigation' );
+    register_nav_menu( 'secondary', 'Footer Navigation' );
+    
 
-// } 
+} 
 
-// add_action('init', 'newTheme_theme_setup_melody8988');
+add_action('after_setup_theme', 'newTheme_theme_setup');
